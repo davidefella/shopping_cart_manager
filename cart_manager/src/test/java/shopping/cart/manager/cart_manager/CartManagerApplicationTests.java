6cf3c142e95becd6ccab9f001bf2ccaee6cee4b3
Davide Fella <davide.fella@gmail.com>
2020-12-11 00:21:40 +0100

    Maven Spring Boot init, gitignore updated

---
+++ cart_manager/src/test/java/shopping/cart/manager/cart_manager/CartManagerApplicationTests.java
@@ -0,0 +1,10 @@
+package shopping.cart.manager.cart_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartManagerApplicationTests {
+
+
+}

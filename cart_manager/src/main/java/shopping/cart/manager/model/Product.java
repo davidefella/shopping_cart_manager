85b6a17df0128dca122e84cd1ddd1f1e427ae4ff
Davide Fella <davide.fella@gmail.com>
2020-12-11 12:58:29 +0100

    Add data model

---
+++ cart_manager/src/main/java/shopping/cart/manager/model/Product.java
@@ -0,0 +1,32 @@
+package shopping.cart.manager.model;
+
+public class Product{
+
+    String name; 
+    int quantity; 
+    double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

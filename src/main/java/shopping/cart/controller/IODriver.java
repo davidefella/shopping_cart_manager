f1b59ee6200b320d08ad9afaf6f4f78e7e5c551a
Davide Fella <davide.fella@gmail.com>
2020-12-13 16:13:08 +0100

    Welcome service

--- src/main/java/shopping/cart/controller/IODriver.java
+++ src/main/java/shopping/cart/controller/IODriver.java
@@ -1,8 +1,13 @@
 package shopping.cart.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +21,25 @@ import shopping.cart.model.ShoppingCart;
 public class IODriver {
 
     @Autowired
-    ShoppingCartManager shoppingCartManager; 
+    ShoppingCartManager shoppingCartManager;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${welcomeMessage}")
+    private String welcomeMessage;
 
     @RequestMapping(value = "/buy_goods", method = RequestMethod.POST)
     public ResponseEntity<CartResult> buyGoods(@RequestBody ShoppingCart shoppingCart) {
 
-        return new ResponseEntity<>( shoppingCartManager.buyGoods(shoppingCart), HttpStatus.OK);
+        return new ResponseEntity<>(shoppingCartManager.buyGoods(shoppingCart), HttpStatus.OK);
     }
+
+    @GetMapping
+    @RequestMapping("/")
+    public String getStatus() {
+
+        return welcomeMessage.concat("<br> <br> Application version: " + appVersion);
+    }
+
 }
